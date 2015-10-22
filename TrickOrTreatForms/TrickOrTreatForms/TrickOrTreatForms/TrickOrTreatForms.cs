aa479a9f0f7bbc8e7728147dfa7562cb1c7d5f46
Malte Götz <maltegoetz@outlook.com>
2015-10-22 22:46:58 +0200

    craeated Pages

--- TrickOrTreatForms/TrickOrTreatForms/TrickOrTreatForms/TrickOrTreatForms.cs
+++ TrickOrTreatForms/TrickOrTreatForms/TrickOrTreatForms/TrickOrTreatForms.cs
@@ -4,38 +4,27 @@ using Xamarin.Forms;
 
 namespace TrickOrTreatForms
 {
-	public class App : Application
-	{
-		public App()
-		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
-		}
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new MainPage();
+        }
 
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
-
