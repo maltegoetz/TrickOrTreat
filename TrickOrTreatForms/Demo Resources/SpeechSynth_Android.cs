c665615e5f92e0c94be2b28a1aa4b194ef9cddd4
Robin-Manuel Thiel <robin-manuel.thiel@thepagedot.de>
2015-10-28 15:27:57 +0100

    Added Demo Resources

---
+++ TrickOrTreatForms/Demo Resources/SpeechSynth_Android.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TrickOrTreatForms.Droid;
+using Android.Speech.Tts;
+using Java.Util;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency (typeof (SpeechSynth_Android))]
+namespace TrickOrTreatForms.Droid
+{
+	public class SpeechSynth_Android : Java.Lang.Object, ISpeechSynth, TextToSpeech.IOnInitListener
+	{
+		TextToSpeech speaker;
+		string toSpeak;
+
+		public SpeechSynth_Android()
+		{
+		}
+
+		#region ISpeechSynth implementation
+
+		public void SpeakText(string text)
+		{
+			toSpeak = text;
+			speaker = new TextToSpeech(Forms.Context, this);
+		}
+
+		#endregion
+
+		public void OnInit(OperationResult status)
+		{
+			if (status.Equals (OperationResult.Success))
+			{
+				var dict = new Dictionary<string,string>();
+				speaker.Speak(toSpeak, QueueMode.Flush, dict);
+			} 
+		}
+	}
+}
+
---
+++ TrickOrTreatForms/Demo Resources/SpeechSynth_iOS.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using AVFoundation;
+using TrickOrTreatForms.iOS;
+
+[assembly: Xamarin.Forms.Dependency (typeof (SpeechSynth_iOS))]
+namespace TrickOrTreatForms.iOS
+{	
+	public class SpeechSynth_iOS : ISpeechSynth
+	{
+		public SpeechSynth_iOS()
+		{
+		}
+
+		#region ISpeechSynth implementation
+
+		public void SpeakText(string text)
+		{
+			var speechSynthesizer = new AVSpeechSynthesizer ();
+			var speechUtterance = new AVSpeechUtterance(text);
+			speechSynthesizer.SpeakUtterance (speechUtterance);
+		}
+
+		#endregion
+	}
+}
+
